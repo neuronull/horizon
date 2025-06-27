b110bc522edb40905621783c2ae54fcf50a9864b
neuronull <neuronull@pm.me>
2025-06-27 13:46:44 -0600

    added button in gui to retrieve forecast

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,10 @@
 use serde::{Deserialize, Serialize};
+use tokio::runtime::Runtime;
 
 use eframe::{CreationContext, Frame};
 use egui::Context;
+
+use lib_weather::fetch_forecast;
 
 #[derive(Deserialize, Serialize)]
 pub struct WeatherApp {}
@@ -31,5 +34,32 @@ impl eframe::App for WeatherApp {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    fn update(&mut self, _ctx: &Context, _frame: &mut Frame) {}
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        egui::SidePanel::right("right_panel")
+            .resizable(false)
+            .default_width(150.0)
+            .show(ctx, |ui| {
+                ui.vertical_centered(|ui| {
+                    ui.heading("Location");
+                });
+                ui.separator();
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    if ui.button("Fetch").clicked() {
+                        let rt = Runtime::new().unwrap();
+                        let api_key = std::env!("PIRATEWEATHER_API_KEY");
+                        let result = rt.block_on(fetch_forecast(&api_key, 37.233, -115.800));
+
+                        if let Ok(data) = result {
+                            println!("{:#?}", data);
+                        } else if let Err(err) = result {
+                            println!("{err}");
+                        }
+                    }
+                });
+            });
+
+        egui::CentralPanel::default().show(ctx, |_ui| {
+            // widgets
+        });
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![warn(clippy::all, rust_2018_idioms)]
 
 mod app;
+
 pub use app::WeatherApp;
 
 pub const APP_NAME: &str = "horizon";
