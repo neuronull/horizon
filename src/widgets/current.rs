6bb6d7806fbf68d3131a77d1efcba4f6c4d848b2
neuronull <neuronull@pm.me>
2025-07-21 11:35:10 -0600

    add apparent temp to current widget

--- src/widgets/current.rs
+++ src/widgets/current.rs
@@ -1,12 +1,16 @@
-use egui::{Color32, Context, Ui, Window};
+use egui::{Color32, Context, Grid, Ui, Window};
 use lib_weather::WeatherData;
 
 use super::{View, Widget};
 
 #[derive(Default)]
 pub struct CurrentWidget {
+    // current temp
     current_temp: Option<i16>,
     current_temp_color: Color32,
+    // feels like
+    feels_like: Option<i16>,
+    feels_like_color: Color32,
 }
 
 impl Widget for CurrentWidget {
@@ -24,13 +28,22 @@ impl Widget for CurrentWidget {
 
     fn update_data(&mut self, data: &dyn WeatherData) {
         if let Some(cur) = data.current() {
+            // current temp
+            self.current_temp = cur.temperature.map(|t| t.round() as i16);
             if let Some(t) = cur.temperature {
-                self.current_temp = Some(t.round() as i16);
                 self.current_temp_color = color_of_temp(t);
             } else {
-                self.current_temp = None;
                 // TODO: choose color
                 self.current_temp_color = Color32::from_rgb(255, 255, 255);
+            }
+
+            // feels like
+            self.feels_like = cur.apparent_temperature.map(|t| t.round() as i16);
+            if let Some(t) = cur.apparent_temperature {
+                self.feels_like_color = color_of_temp(t);
+            } else {
+                // TODO: choose color
+                self.feels_like_color = Color32::from_rgb(255, 255, 255);
             }
         }
     }
@@ -38,11 +51,29 @@ impl Widget for CurrentWidget {
 
 impl View for CurrentWidget {
     fn ui(&mut self, ui: &mut Ui) {
-        if let Some(t) = self.current_temp {
-            ui.colored_label(self.current_temp_color, format!("{t}"));
-        } else {
-            ui.colored_label(self.current_temp_color, "--");
-        }
+        Grid::new("current_grid")
+            .num_columns(2)
+            .spacing([40.0, 4.0])
+            .striped(true)
+            .show(ui, |ui| {
+                // current temp
+                ui.label("Current temperature (F): ");
+                if let Some(t) = self.current_temp {
+                    ui.colored_label(self.current_temp_color, format!("{t}"));
+                } else {
+                    ui.colored_label(self.current_temp_color, "--");
+                }
+                ui.end_row();
+
+                // feels like
+                ui.label("Feels like (F): ");
+                if let Some(t) = self.feels_like {
+                    ui.colored_label(self.feels_like_color, format!("{t}"));
+                } else {
+                    ui.colored_label(self.feels_like_color, "--");
+                }
+                ui.end_row();
+            });
     }
 }
 
