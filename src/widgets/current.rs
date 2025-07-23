c13ec3cfe76b01d01c0620b9c4cb31c6d1ee7ee9
neuronull <neuronull@pm.me>
2025-07-23 15:32:14 -0600

    refactor clean up current widget

--- src/widgets/current.rs
+++ src/widgets/current.rs
@@ -6,25 +6,25 @@ use super::{View, Widget};
 #[derive(Default)]
 pub struct CurrentWidget {
     // current temp
-    current_temp: Option<i16>,
+    current_temp: String,
     current_temp_color: Color32,
     // feels like
-    feels_like: Option<i16>,
+    feels_like: String,
     feels_like_color: Color32,
     // precipitation
-    precipitation_prob: Option<i64>,
+    precipitation_prob: String,
     // humidity
-    humidity: Option<i64>,
+    humidity: String,
     // pressure
-    pressure: Option<f64>,
+    pressure: String,
     // wind
-    wind: Option<f64>,
+    wind: String,
     // uv index
-    uv_index: Option<f64>,
+    uv_index: String,
     // cloud cover
-    cloud_cover: Option<i64>,
+    cloud_cover: String,
     // visibility
-    visibility: Option<f64>,
+    visibility: String,
 }
 
 impl Widget for CurrentWidget {
@@ -42,40 +42,52 @@ impl Widget for CurrentWidget {
 
     fn update_data(&mut self, data: &dyn WeatherData) {
         if let Some(cur) = data.current() {
-            // current temp
-            self.current_temp = cur.temperature.map(|t| t.round() as i16);
-            if let Some(t) = cur.temperature {
-                self.current_temp_color = color_of_temp(t);
+            self.current_temp = cur
+                .temperature
+                .map_or("--".to_string(), |v| format!("{}", v.round()));
+
+            self.current_temp_color = if let Some(t) = cur.temperature {
+                color_of_temp(t)
             } else {
-                self.current_temp_color = Color32::from_rgb(255, 255, 255);
-            }
+                Color32::from_rgb(255, 255, 255)
+            };
 
             // feels like
-            self.feels_like = cur.apparent_temperature.map(|t| t.round() as i16);
-            if let Some(t) = cur.apparent_temperature {
-                self.feels_like_color = color_of_temp(t);
+            self.feels_like = cur
+                .apparent_temperature
+                .map_or("--".to_string(), |v| format!("{}", v.round()));
+
+            self.feels_like_color = if let Some(t) = cur.apparent_temperature {
+                color_of_temp(t)
             } else {
-                self.feels_like_color = Color32::from_rgb(255, 255, 255);
-            }
+                Color32::from_rgb(255, 255, 255)
+            };
 
-            self.precipitation_prob = cur.precip_probability.map(|p| (p * 100.0).round() as i64);
-            dbg!(cur.precip_probability);
+            self.precipitation_prob = float_to_percent_str(cur.precip_probability);
 
-            self.pressure = cur.pressure;
+            self.pressure = float_str(cur.pressure);
 
-            self.humidity = cur.humidity.map(|h| (h * 100.0).round() as i64);
-            dbg!(cur.humidity);
+            self.humidity = float_to_percent_str(cur.humidity);
 
-            self.wind = cur.wind_speed;
+            self.wind = float_str(cur.wind_speed);
 
-            self.uv_index = cur.uv_index;
+            self.uv_index = float_str(cur.uv_index);
 
-            self.cloud_cover = cur.cloud_cover.map(|c| (c * 100.0).round() as i64);
-            dbg!(cur.cloud_cover);
+            self.cloud_cover = float_to_percent_str(cur.cloud_cover);
 
-            self.visibility = cur.visibility;
+            self.visibility = float_str(cur.visibility);
         }
     }
+}
+
+fn float_to_percent_str(f: Option<f64>) -> String {
+    f.map_or("--".to_string(), |v| {
+        format!("{}", (v * 100.0).round() as i64)
+    })
+}
+
+fn float_str(f: Option<f64>) -> String {
+    f.map_or("--".to_string(), |v| format!("{v:.1}"))
 }
 
 impl View for CurrentWidget {
@@ -85,84 +97,40 @@ impl View for CurrentWidget {
             .spacing([40.0, 4.0])
             .striped(true)
             .show(ui, |ui| {
-                // current temp
                 ui.label("Current temperature (F): ");
-                if let Some(t) = self.current_temp {
-                    ui.colored_label(self.current_temp_color, format!("{t}"));
-                } else {
-                    ui.colored_label(self.current_temp_color, "--");
-                }
+                ui.colored_label(self.current_temp_color, &self.current_temp);
                 ui.end_row();
 
-                // feels like
                 ui.label("Feels like (F): ");
-                if let Some(t) = self.feels_like {
-                    ui.colored_label(self.feels_like_color, format!("{t}"));
-                } else {
-                    ui.colored_label(self.feels_like_color, "--");
-                }
+                ui.colored_label(self.feels_like_color, &self.feels_like);
                 ui.end_row();
 
-                // precipitation
                 ui.label("Percipitation prob (%): ");
-                ui.label(
-                    self.precipitation_prob
-                        .map_or("--".to_string(), |v| format!("{v}"))
-                        .to_string(),
-                );
+                ui.label(&self.precipitation_prob);
                 ui.end_row();
 
-                // humidity
                 ui.label("Humidity (%): ");
-                ui.label(
-                    self.humidity
-                        .map_or("--".to_string(), |v| format!("{v}"))
-                        .to_string(),
-                );
+                ui.label(&self.humidity);
                 ui.end_row();
 
                 ui.label("Pressure (millibars): ");
-                ui.label(
-                    self.pressure
-                        .map_or("--".to_string(), |v| format!("{v:.1}"))
-                        .to_string(),
-                );
+                ui.label(&self.pressure);
                 ui.end_row();
 
-                // wind (mph)
                 ui.label("Wind (mph): ");
-                ui.label(
-                    self.wind
-                        .map_or("--".to_string(), |v| format!("{v:.1}"))
-                        .to_string(),
-                );
+                ui.label(&self.wind);
                 ui.end_row();
 
-                // uv index ()
                 ui.label("UV Index: ");
-                ui.label(
-                    self.uv_index
-                        .map_or("--".to_string(), |v| format!("{v:.1}"))
-                        .to_string(),
-                );
+                ui.label(&self.uv_index);
                 ui.end_row();
 
-                // cloud cover (%)
                 ui.label("Cloud cover (%): ");
-                ui.label(
-                    self.cloud_cover
-                        .map_or("--".to_string(), |v| format!("{v}"))
-                        .to_string(),
-                );
+                ui.label(&self.cloud_cover);
                 ui.end_row();
 
-                // visibility (miles)
                 ui.label("Visibility (miles): ");
-                ui.label(
-                    self.visibility
-                        .map_or("--".to_string(), |v| format!("{v:.1}"))
-                        .to_string(),
-                );
+                ui.label(&self.visibility);
                 ui.end_row();
             });
     }
