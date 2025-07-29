48373acdc91c43d064562a56a7e01c8dbfa5b23b
neuronull <neuronull@pm.me>
2025-07-29 12:48:31 -0600

    start support for geolocate

---
+++ lib/geolocate/src/lib.rs
@@ -0,0 +1,69 @@
+use anyhow::Error;
+use serde::Deserialize;
+
+const IPAPI_URL: &str = "https://api.ipgeolocation.io/v2/ipgeo";
+
+#[derive(Default, Debug, Deserialize)]
+pub struct GeoResponse {
+    pub ip: String,
+    pub location: Location,
+    pub country_metadata: CountryMetadata,
+    pub currency: Currency,
+}
+
+#[derive(Default, Debug, Deserialize)]
+pub struct Location {
+    pub continent_code: String,
+    pub continent_name: String,
+    pub country_code2: String,
+    pub country_code3: String,
+    pub country_name: String,
+    pub country_name_official: String,
+    pub country_capital: String,
+    pub state_prov: String,
+    pub state_code: String,
+    pub district: String,
+    pub city: String,
+    pub zipcode: String,
+    pub latitude: String,
+    pub longitude: String,
+    pub is_eu: bool,
+    pub country_flag: String,
+    pub geoname_id: String,
+    pub country_emoji: String,
+}
+
+#[derive(Default, Debug, Deserialize)]
+pub struct CountryMetadata {
+    pub calling_code: String,
+    pub tld: String,
+    pub languages: Vec<String>,
+}
+
+#[derive(Default, Debug, Deserialize)]
+pub struct Currency {
+    pub code: String,
+    pub name: String,
+    pub symbol: String,
+}
+
+pub async fn get_geo_location() -> Result<GeoResponse, Error> {
+    let api_key = env!("IPLOCATE_API_KEY");
+    let url = format!("{IPAPI_URL}?apiKey={api_key}");
+
+    #[cfg(target_arch = "wasm32")]
+    {
+        use gloo_net::http::Request;
+
+        Ok(Request::get(&url)
+            .send()
+            .await?
+            .json::<GeoResponse>()
+            .await?)
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        Ok(reqwest::get(url).await?.json::<GeoResponse>().await?)
+    }
+}
--- src/view/weather.rs
+++ src/view/weather.rs
@@ -1,11 +1,18 @@
 use std::collections::BTreeSet;
 
+use anyhow::Result;
 use egui::{Id, Label, Layout, Modal, ScrollArea, TextEdit, Ui};
+use tokio::{
+    runtime::Handle,
+    sync::watch::{self, Receiver, Sender},
+};
+use tracing::{error, info};
 
 use crate::{FetchState, Widgets, A51_LAT, A51_LON};
+use lib_geolocate::{get_geo_location, GeoResponse};
 
-#[derive(Default)]
 pub struct WeatherView {
+    rt: Option<Handle>,
     /// UI widgets
     pub widgets: Widgets,
     open_widgets: BTreeSet<String>,
@@ -14,20 +21,56 @@ pub struct WeatherView {
     /// location longitude
     pub longitude_str: String,
     pub location_error_modal_open: bool,
+    sender: Sender<Result<GeoResponse>>,
+    receiver: Receiver<Result<GeoResponse>>,
 }
 
 impl WeatherView {
     #[must_use]
-    pub fn new() -> Self {
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn new(rt: Handle) -> Self {
+        let (sender, receiver) = watch::channel(Ok(GeoResponse::default()));
         Self {
+            rt: Some(rt),
+            widgets: Widgets::new(),
+            open_widgets: BTreeSet::new(),
             latitude_str: String::from(A51_LAT),
             longitude_str: String::from(A51_LON),
+            location_error_modal_open: false,
+            sender,
+            receiver,
+        }
+    }
+
+    #[must_use]
+    #[cfg(target_arch = "wasm32")]
+    pub fn new() -> Self {
+        let (sender, receiver) = watch::channel(Ok(GeoResponse::default()));
+        Self {
+            rt: None,
             widgets: Widgets::new(),
-            ..Default::default()
+            open_widgets: BTreeSet::new(),
+            latitude_str: String::from(A51_LAT),
+            longitude_str: String::from(A51_LON),
+            location_error_modal_open: false,
+            sender,
+            receiver,
         }
     }
 
     pub fn update(&mut self, ui: &mut Ui, fetch_state: &mut FetchState) {
+        if let Ok(true) = self.receiver.has_changed() {
+            match &*self.receiver.borrow_and_update() {
+                Ok(geo) => {
+                    self.latitude_str = geo.location.latitude.to_owned();
+                    self.longitude_str = geo.location.longitude.to_owned();
+                }
+                Err(err) => {
+                    error!("{err}");
+                }
+            }
+        }
+
         egui::SidePanel::right("right_panel")
             .resizable(false)
             .show(ui.ctx(), |ui| {
@@ -68,14 +111,42 @@ impl WeatherView {
                 ui.add(TextEdit::singleline(&mut self.longitude_str).hint_text(A51_LON));
 
                 ui.end_row();
+
+                if ui.button("Geolocate").clicked() {
+                    self.request_locate();
+                }
+                if ui.button("Fetch").clicked() && *fetch_state == FetchState::Completed {
+                    *fetch_state = FetchState::Requested;
+                }
+                ui.end_row();
             });
 
-        egui::ScrollArea::vertical().show(ui, |ui| {
-            if ui.button("Fetch").clicked() && *fetch_state == FetchState::Completed {
-                *fetch_state = FetchState::Requested;
-            }
-        });
         ui.separator();
+    }
+
+    fn request_locate(&mut self) {
+        info!("Geolocating...");
+        let sender = self.sender.clone();
+
+        cfg_if::cfg_if! {
+            if #[cfg(target_arch = "wasm32")] {
+                wasm_bindgen_futures::spawn_local(async move {
+                    let result = get_geo_location().await;
+                    if let Err(err) = sender.send(result) {
+                        error!("{err}");
+                    } else {
+                        info!("Geolocate success.");
+                    }
+                });
+            } else {
+                let result = self.rt.as_ref().unwrap().block_on(get_geo_location());
+                if let Err(err) = sender.send(result) {
+                    error!("{err}");
+                } else {
+                    info!("Geolocate success.");
+                }
+            }
+        }
     }
 
     fn show_location_error_modal(&mut self, ui: &mut Ui) {
